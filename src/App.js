9690250068ee3245e58cdc76528884aed02190de
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-04-12 10:14:09 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    axios.get('https://reqres.in/api/users?page=2').then(res=>{
+      setUsers(res.data.data)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Santiago's Resume</h1>
+        <pre>
+          {JSON.stringify(users)}
+        </pre>
     </div>
   );
 }
-
 export default App;
